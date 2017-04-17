5d4c61065e0904c5c40c344110a13ccb18f1481b
Omar Alhadlaq <o.hadlag@gmail.com>
2017-04-16 19:57:11 -0700

    added model

---
+++ model.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow.contrib.rnn import DropoutWrapper
+from tensorflow.contrib.rnn import GRUCell
+
+
+class Model:
+    def __init__(self, config):
+        self.keep_prob = config.keep_prob
+        self.dim = config.hidden_size
+
+        # embeddings matrix
+        self.emb_mat = tf.slice(config.emb_mat, [2, 0], [-1, -1])
+        self.emb_mat = tf.concat([tf.get_variable('emb_mat', shape=[2, self.dim]), self.emb_mat], axis=0)
+
+        self.max_x = config.max_context_size
+        self.max_q = config.max_ques_size
+
+    def compute(self, x, x_len, q, q_len):
+        # tensor of boolean values of max_x length and True in first x_len indices
+        x_mask = tf.sequence_mask(x_len, self.max_x)
+        q_mask = tf.sequence_mask(q_len, self.max_q)
+
+        context = tf.nn.embedding_lookup(self.emb_mat, x, name='context')
+        question = tf.nn.embedding_lookup(self.emb_mat, q, name='question')
+
+        with tf.variable_scope('pre_process'):
+            cell = GRUCell(self.dim)
+            cell = DropoutWrapper(cell, input_keep_prob=self.keep_prob)  # to avoid over-fitting
+
+            outputs_context, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, inputs=context, sequence_length=x_len)
+            context_fw, context_bw = outputs_context
+            context_output = tf.concat([context_fw, context_bw], axis=2)
+
+            tf.get_variable_scope().reuse_variables()
+            outputs_question, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, inputs=question, sequence_length=q_len)
+            question_fw, question_bw = outputs_question
+            question_output = tf.concat([question_fw, question_bw], axis=2)
+
+        mask = tf.expand_dims(q_mask, -1)
+        q_temp = question_output * tf.cast(mask, 'float')
+        q_avg = tf.reduce_mean(q_temp, axis=1)
+        q_avg_exp = tf.expand_dims(q_avg, axis=1)
+        q_avg_tiled = tf.tile(q_avg_exp, [1, self.max_x, 1])
+
+        xq = tf.concat([context_output, q_avg_tiled, context_output * q_avg_tiled], axis=2)
+
+        with tf.variable_scope("post_process"):
+            cell = GRUCell(self.dim)
+            cell = DropoutWrapper(cell, input_keep_prob=self.keep_prob)  # to avoid over-fitting
+
+            outputs_xq, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, inputs=xq, sequence_length=x_len)
+            xq_fw, xq_bw = outputs_xq
+            xq_output = tf.concat([xq_fw, xq_bw], axis=2)
+
+        xq_flat = tf.reshape(xq_output, [-1, 2 * self.dim])
+
+        # logits
+        with tf.variable_scope('start'):
+            val = tf.reshape(tf.layers.dense(inputs=xq_flat, units=1), [-1, self.max_x])
+            logits_start = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
+            yp_start = tf.argmax(logits_start, axis=1)
+
+        with tf.variable_scope('end'):
+            val = tf.reshape(tf.layers.dense(inputs=xq_flat, units=1), [-1, self.max_x])
+            logits_end = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
+            yp_end = tf.argmax(logits_end, axis=1)
+
+        outputs = {'logits_start': logits_start, 'logits_end': logits_end, 'yp_start': yp_start, 'yp_end': yp_end}
+        return outputs
