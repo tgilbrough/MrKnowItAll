1d0eee63dbbcc6637f1d630dfac781921e4e5a83
TJ Gilbrough <tgilbrough@yahoo.com>
2017-04-18 00:25:27 -0700

    Made split_data do train and dev at once

--- datasets/msmarco/split_data.py
+++ datasets/msmarco/split_data.py
@@ -4,28 +4,27 @@ import os
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data-file', default='./train_v1.1.json')
-    parser.add_argument("--dest-dir", default='./train/')
+    parser.add_argument('--train_path', default='./train_v1.1.json')
+    parser.add_argument('--train_dir', default='./train/')
+    parser.add_argument('--dev_path', default='./dev_v1.1.json')
+    parser.add_argument('--dev_dir', default='./dev/')
     
     return parser
 
-def main():
-    parser = get_parser()
-    config = parser.parse_args()
-
+def split(path, dest_dir):
     question_files = {}
 
-    if not os.path.exists(config.dest_dir):
-        os.makedirs(config.dest_dir)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-    with open(config.data_file, encoding='utf-8') as data_file:    
+    with open(path, encoding='utf-8') as data_file:    
         for line in data_file:
             question = json.loads(line)
             answer_type = question['query_type']
 
             # Initialization
             if answer_type not in question_files:
-                question_files[answer_type] = open(config.dest_dir + answer_type + '.json', 'w', encoding='utf-8')
+                question_files[answer_type] = open(dest_dir + answer_type + '.json', 'w', encoding='utf-8')
 
             # Print question out to appropriate file
             print(line, end='', file=question_files[answer_type])
@@ -34,6 +33,12 @@ def main():
         for answer_type in question_files.keys():
             question_files[answer_type].close()
 
+def main():
+    parser = get_parser()
+    config = parser.parse_args()
+
+    split(config.train_path, config.train_dir)
+    split(config.dev_path, config.dev_dir)
 
 if __name__ == "__main__":
     main()
