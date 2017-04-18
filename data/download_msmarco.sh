cd6c9bafa38f3658977e013f36d2ddfc2fe38a44
Lane Aasen <laneaasen@gmail.com>
2017-04-17 18:31:09 -0700

    updates download_msmarco to split the datasets

--- data/download_msmarco.sh
+++ data/download_msmarco.sh
@@ -2,6 +2,7 @@
 echo "Downloading training data..."
 wget https://msmarco.blob.core.windows.net/msmarco/train_v1.1.json.gz -P msmarco
 gunzip msmarco/train_v1.1.json.gz
+python msmarco/split_data.py --data-file msmarco/train_v1.1.json --dest-dir ./msmarco/train/
 
 echo "Downloading testing data..."
 wget https://msmarco.blob.core.windows.net/msmarco/test_public_v1.1.json.gz -P msmarco
@@ -10,3 +11,4 @@ gunzip msmarco/test_public_v1.1.json.gz
 echo "Downloading dev data..."
 wget https://msmarco.blob.core.windows.net/msmarco/dev_v1.1.json.gz -P msmarco
 gunzip msmarco/dev_v1.1.json.gz
+python msmarco/split_data.py --data-file msmarco/dev_v1.1.json --dest-dir ./msmarco/dev/
