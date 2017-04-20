848d2ece43007b4130319f943bbc07bff081b903
Lane Aasen <laneaasen@gmail.com>
2017-04-20 12:35:40 -0700

    updates download_msmarco to reflect changes in split_data

--- datasets/download_msmarco.sh
+++ datasets/download_msmarco.sh
@@ -1,14 +1,17 @@
 
+cd msmarco
 echo "Downloading training data..."
-wget https://msmarco.blob.core.windows.net/msmarco/train_v1.1.json.gz -P msmarco
-gunzip msmarco/train_v1.1.json.gz
-python msmarco/split_data.py --data-file msmarco/train_v1.1.json --dest-dir ./msmarco/train/
+wget https://msmarco.blob.core.windows.net/msmarco/train_v1.1.json.gz
+gunzip train_v1.1.json.gz
 
 echo "Downloading testing data..."
-wget https://msmarco.blob.core.windows.net/msmarco/test_public_v1.1.json.gz -P msmarco
-gunzip msmarco/test_public_v1.1.json.gz
+wget https://msmarco.blob.core.windows.net/msmarco/test_public_v1.1.json.gz
+gunzip test_public_v1.1.json.gz
 
 echo "Downloading dev data..."
-wget https://msmarco.blob.core.windows.net/msmarco/dev_v1.1.json.gz -P msmarco
-gunzip msmarco/dev_v1.1.json.gz
-python msmarco/split_data.py --data-file msmarco/dev_v1.1.json --dest-dir ./msmarco/dev/
+wget https://msmarco.blob.core.windows.net/msmarco/dev_v1.1.json.gz
+gunzip dev_v1.1.json.gz
+
+echo "Splitting queries by type..."
+python split_data.py
+cd ..
