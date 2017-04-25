34678a15887399fa54e7e0bfebdfa881b1652ad1
TJ Gilbrough <tgilbrough@yahoo.com>
2017-04-24 20:16:41 -0700

    Progress on coattention

--- coattention_model.py
+++ coattention_model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.contrib.rnn import DropoutWrapper
 from tensorflow.contrib.rnn import GRUCell, LSTMCell
+import numpy as np
+from tf_ops import highway_maxout, batch_linear
 
 # Attempt at recreating: https://arxiv.org/pdf/1611.01604.pdf
 class Model:
@@ -8,8 +10,8 @@ class Model:
         self.model_name = 'coattention'
         self.emb_size = config.emb_size
         self.hidden_size = config.hidden_size
-        # self.maxout_size = config.maxout_size
-        # self.max_decode_steps = config.max_decode_steps
+        self.pool_size = config.pool_size
+        self.max_decode_steps = config.max_decode_steps
         self.max_x = max_x
         self.max_q = max_q
         self.saver = None
@@ -29,7 +31,6 @@ class Model:
         lstm_enc = LSTMCell(self.hidden_size)
         lstm_enc = DropoutWrapper(lstm_enc, input_keep_prob=keep_prob)
         
-
         with tf.variable_scope('encoding_context'):
             D, _ = tf.nn.dynamic_rnn(lstm_enc, context, sequence_length=x_len, dtype=tf.float32)
             D = tf.transpose(D, perm=[0, 2, 1]) 
@@ -40,18 +41,11 @@ class Model:
             q = tf.transpose(q, perm=[0, 2, 1]) 
             tf.summary.histogram("Q'", q)
 
-            # map to separate question space
-            q_weights = tf.get_variable(name='q_weights', shape=[self.hidden_size, self.hidden_size], dtype=tf.float32)
-            res = tf.map_fn(lambda x: tf.matmul(q_weights, x), q)
-            q_biases = tf.get_variable(name='q_bias', shape=[self.hidden_size, self.max_q], dtype=tf.float32)
-            
-            Q = tf.tanh(tf.map_fn(lambda x: tf.add(x, q_biases), res))    
+            Q = tf.tanh(batch_linear(q, self.hidden_size, True))
             tf.summary.histogram("Q", Q)   
             
-
         print('D:', D.get_shape())
         print('Q:', Q.get_shape())
-        
 
         with tf.variable_scope('affinity_mat'):
             L = tf.matmul(tf.transpose(D, perm=[0, 2, 1]), Q, name='L')
@@ -87,7 +81,6 @@ class Model:
             lstm_fw_cell = DropoutWrapper(lstm_fw_cell, input_keep_prob=keep_prob)
             lstm_bw_cell = DropoutWrapper(lstm_bw_cell, input_keep_prob=keep_prob)
 
-
             u, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs=co_att, sequence_length=x_len, dtype=tf.float32)
     
             U = tf.concat(u, axis=2)
@@ -95,38 +88,76 @@ class Model:
         
         print('U:', U.get_shape())
 
+        def select(u, pos, idx):
+            u_idx = tf.gather(u, idx) # U for batch idx
+            pos_idx = tf.gather(pos, idx) # Start for batch idx
+            return tf.reshape(tf.gather(u_idx, pos_idx), [-1])
 
-        # with tf.variable_scope('selector'):
-        #     highway_alpha = highway_maxout(self.hidden_size, self.maxout_size)
-        #     highway_beta = highway_maxout(self.hidden_size, self.maxout_size)
-        
-        #     loop_until = tf.to_int32(np.array(range(batch_size)))
-        #     # initial estimated positions
-        #     s, e = tf.split(0, 2, self._guesses)
+        with tf.variable_scope('selector'):        
+            
+            batch_size = tf.shape(U)[0]
 
-        #     fn = lambda idx: select(U, s, idx)
-        #     u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            # batch indices
+            loop_until = tf.range(0, batch_size, dtype=tf.int32)
 
-        #     fn = lambda idx: select(U, e, idx)
-        #     u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            # initial estimated positions
+            initial_guesses = tf.zeros([2, batch_size], dtype=tf.int32)
+            s, e = tf.split(initial_guesses, 2, axis=0)
 
-        
-        # with tf.variable_scope('decoder'):
+            print('s:', s.get_shape())
 
-        with tf.variable_scope('post_process'):
-            gru_xq_cell = GRUCell(self.hidden_size)
-            gru_xq_cell = DropoutWrapper(gru_xq_cell, input_keep_prob=keep_prob)  # to avoid over-fitting
+            # Get U vectors of starting indexes
+            fn = lambda idx: select(U, s, idx)
+            u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            u_s = tf.reshape(u_s, shape=[batch_size, 2 * self.hidden_size])
 
-            outputs_xq, _ = tf.nn.bidirectional_dynamic_rnn(gru_xq_cell, gru_xq_cell, inputs=tf.transpose(U, perm=[0, 2, 1]), sequence_length=x_len, dtype=tf.float32)
-            xq_fw, xq_bw = outputs_xq
-            xq_output = tf.concat([xq_fw, xq_bw], axis=2)
-            tf.summary.histogram("xq_output", xq_output)
+            # Get U vectors of ending indexes
+            fn = lambda idx: select(U, e, idx)
+            u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            u_e = tf.reshape(u_e, shape=[batch_size, 2 * self.hidden_size])
 
-        # Get rid of the sequence dimension
-        xq_flat = tf.reshape(xq_output, [-1, 2 * self.hidden_size])
+        print('u_s:', u_s.get_shape())
+        print('u_e:', u_e.get_shape())
 
-        # tensor of boolean values of max_x length and True in first x_len indices
-        x_mask = tf.sequence_mask(x_len, self.max_x)
+        with tf.variable_scope('highway_init'):
+            highway_alpha = highway_maxout(self.hidden_size, self.pool_size)
+            highway_beta = highway_maxout(self.hidden_size, self.pool_size)
+
+        with tf.variable_scope('decoder'):
+            # LSTM for decoding
+            lstm_dec = LSTMCell(self.hidden_size)
+
+            for step in range(self.max_decode_steps):
+                if step > 0:
+                    tf.reuse_variables()
+                # single step lstm
+                _input = tf.concat([u_s, u_e], axis=1)
+                print('_input:', _input.get_shape())
+
+                _, h = tf.contrib.rnn.static_rnn(lstm_dec, [_input], dtype=tf.float32)
+               
+                h_state = tf.concat(h, axis=1)
+
+                print('h_state:', h_state.get_shape())
+                with tf.variable_scope('highway_alpha'):
+                    # compute start position first
+                    fn = lambda u_t: highway_alpha(u_t, h_state, u_s, u_e)
+                    alpha = tf.map_fn(lambda u_t: fn(u_t), U, dtype=tf.float32)
+                    s = tf.reshape(tf.argmax(alpha, axis=0), [batch_size])
+                    
+                    # update start guess
+                    fn = lambda idx: select(U, s, idx)
+                    u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+
+                with tf.variable_scope('highway_beta'):
+                    # compute end position next
+                    fn = lambda u_t: highway_beta(u_t, h_state, u_s, u_e)
+                    beta = tf.map_fn(lambda u_t: fn(u_t), U, dtype=tf.float32)
+                    e = tf.reshape(tf.argmax(beta, axis=0), [batch_size])
+                    
+                    # update end guess
+                    fn = lambda idx: select(U, e, idx)
+                    u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
 
         # logits
         with tf.variable_scope('start_index'):
@@ -143,3 +174,5 @@ class Model:
 
         outputs = {'logits_start': logits_start, 'logits_end': logits_end, 'yp_start': yp_start, 'yp_end': yp_end}
         return outputs
+
+    
