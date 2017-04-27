34fe07445185460ed70c2e578728a2c99cbf084f
TJ Gilbrough <tgilbrough@yahoo.com>
2017-04-26 18:09:44 -0700

    Adding loss and summaries to model file

--- attention_model.py
+++ attention_model.py
@@ -11,11 +11,11 @@ class Model:
         self.max_q = max_q
         self.saver = None
 
-    def build(self, x, x_len, q, q_len, embeddings, keep_prob):
+    def build(self, x, x_len, q, q_len, y_begin, y_end, embeddings, keep_prob):
         with tf.variable_scope('embedding_matrix'):
             # embeddings matrix, may be memory ineffecient (Fix)
-            emb_mat = tf.get_variable(name="emb_mat", shape=embeddings.shape, initializer=tf.constant_initializer(embeddings), trainable=False)
-            tf.summary.histogram("emb_mat", emb_mat)
+            emb_mat = tf.get_variable(name='emb_mat', shape=embeddings.shape, initializer=tf.constant_initializer(embeddings), trainable=False)
+            tf.summary.histogram('emb_mat', emb_mat)
         
         with tf.variable_scope('embedding_context'):
             context = tf.nn.embedding_lookup(emb_mat, x, name='context')
@@ -30,19 +30,19 @@ class Model:
             outputs_context, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, inputs=context, sequence_length=x_len, dtype=tf.float32)
             context_fw, context_bw = outputs_context
             context_output = tf.concat([context_fw, context_bw], axis=2)
-            tf.summary.histogram("context_output", context_output)
+            tf.summary.histogram('context_output', context_output)
 
             tf.get_variable_scope().reuse_variables()
 
             outputs_question, _ = tf.nn.bidirectional_dynamic_rnn(cell, cell, inputs=question, sequence_length=q_len, dtype=tf.float32)
             question_fw, question_bw = outputs_question
             question_output = tf.concat([question_fw, question_bw], axis=2)
-            tf.summary.histogram("question_output", question_output)
+            tf.summary.histogram('question_output', question_output)
 
         q_mask = tf.sequence_mask(q_len, self.max_q)
         x_mask = tf.sequence_mask(x_len, self.max_x)
 
-        with tf.variable_scope("attention"):
+        with tf.variable_scope('attention'):
             x_exp = tf.tile(tf.expand_dims(context_output, axis=2), [1, 1, self.max_q, 1])
             q_exp = tf.tile(tf.expand_dims(question_output, axis=1), [1, self.max_x, 1, 1])
             inputs = tf.concat([x_exp, q_exp, x_exp * q_exp], 3)
@@ -51,7 +51,7 @@ class Model:
             logits = val - (1.0 - tf.cast(mask, 'float')) * 10.0e10
             probs = tf.nn.softmax(logits)
             sum_q = tf.reduce_sum(q_exp * tf.expand_dims(probs, -1), axis=2)
-            tf.summary.histogram("sum_q", sum_q)
+            tf.summary.histogram('sum_q', sum_q)
 
         xq = tf.concat([context_output, sum_q, context_output * sum_q], axis=2)
 
@@ -62,7 +62,7 @@ class Model:
             outputs_xq, _ = tf.nn.bidirectional_dynamic_rnn(gru_xq_cell, gru_xq_cell, inputs=xq, sequence_length=x_len, dtype=tf.float32)
             xq_fw, xq_bw = outputs_xq
             xq_output = tf.concat([xq_fw, xq_bw], axis=2)
-            tf.summary.histogram("xq_output", xq_output)
+            tf.summary.histogram('xq_output', xq_output)
 
         # Get rid of the sequence dimension
         xq_flat = tf.reshape(xq_output, [-1, 2 * self.dim])
@@ -75,13 +75,27 @@ class Model:
             val = tf.reshape(tf.layers.dense(inputs=xq_flat, units=1), [-1, self.max_x])
             logits_start = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
             yp_start = tf.argmax(logits_start, axis=1, name='starting_index')
-            tf.summary.histogram("yp_start", yp_start)
+            tf.summary.histogram('yp_start', yp_start)
 
         with tf.variable_scope('end_index'):
             val = tf.reshape(tf.layers.dense(inputs=xq_flat, units=1), [-1, self.max_x])
             logits_end = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
             yp_end = tf.argmax(logits_end, axis=1, name='ending_index')
-            tf.summary.histogram("yp_end", yp_end)
+            tf.summary.histogram('yp_end', yp_end)
 
-        outputs = {'logits_start': logits_start, 'logits_end': logits_end, 'yp_start': yp_start, 'yp_end': yp_end}
-        return outputs
+        with tf.variable_scope('loss'):
+            loss1 = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_begin, logits=logits_start), name='beginning_loss')
+            loss2 = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_end, logits=logits_end), name='ending_loss')
+            loss = loss1 + loss2
+        with tf.variable_scope('accuracy'):
+            acc1 = tf.reduce_mean(tf.cast(tf.equal(y_begin, tf.cast(tf.argmax(logits_start, 1), 'int32')), 'float'), name='beginning_accuracy')
+            acc2 = tf.reduce_mean(tf.cast(tf.equal(y_end, tf.cast(tf.argmax(logits_end, 1), 'int32')), 'float'), name='ending_accuracy')
+
+        tf.summary.scalar('loss', loss)
+        tf.summary.scalar('loss1', loss1)
+        tf.summary.scalar('loss2', loss2)
+        tf.summary.scalar('accuracy1', acc1)
+        tf.summary.scalar('accuracy2', acc2)
+
+        self.loss = loss
+        self.merged_summary = tf.summary.merge_all()
--- coattention_model.py
+++ coattention_model.py
@@ -16,62 +16,65 @@ class Model:
         self.max_q = max_q
         self.saver = None
 
-    def build(self, x, x_len, q, q_len, embeddings, keep_prob):
+    def build(self, x, x_len, q, q_len, y_begin, y_end, embeddings, keep_prob):
         with tf.variable_scope('embedding_matrix'):
             # embeddings matrix, may be memory ineffecient (Fix)
-            emb_mat = tf.get_variable(name="emb_mat", shape=embeddings.shape, initializer=tf.constant_initializer(embeddings), trainable=False)
-            tf.summary.histogram("emb_mat", emb_mat)
+            emb_mat = tf.get_variable(name='emb_mat', shape=embeddings.shape, initializer=tf.constant_initializer(embeddings), trainable=False)
+            tf.summary.histogram('emb_mat', emb_mat)
         
         with tf.variable_scope('embedding_context'):
-            context = tf.nn.embedding_lookup(emb_mat, x, name='context')
+            context = tf.nn.embedding_lookup(emb_mat, x, name='context') # (batch_size, max_x, emb_size)
 
         with tf.variable_scope('embedding_question'):
-            question = tf.nn.embedding_lookup(emb_mat, q, name='question')
+            question = tf.nn.embedding_lookup(emb_mat, q, name='question') # (batch_size, max_q, emb_size)
 
         lstm_enc = LSTMCell(self.hidden_size)
         lstm_enc = DropoutWrapper(lstm_enc, input_keep_prob=keep_prob)
         
         with tf.variable_scope('encoding_context'):
-            D, _ = tf.nn.dynamic_rnn(lstm_enc, context, sequence_length=x_len, dtype=tf.float32)
-            D = tf.transpose(D, perm=[0, 2, 1]) 
-            tf.summary.histogram("D", D)
+            D, _ = tf.nn.dynamic_rnn(lstm_enc, context, sequence_length=x_len, dtype=tf.float32) # (batch_size, max_x, hidden_size)
+            D = tf.transpose(D, perm=[0, 2, 1]) # (batch_size, hidden_size, max_x)
+            tf.summary.histogram('D', D)
 
         with tf.variable_scope('encoding_question'):
-            q, _ = tf.nn.dynamic_rnn(lstm_enc, question, sequence_length=q_len, dtype=tf.float32)
-            q = tf.transpose(q, perm=[0, 2, 1]) 
+            q, _ = tf.nn.dynamic_rnn(lstm_enc, question, sequence_length=q_len, dtype=tf.float32) # (batch_size, max_q, hidden_size)
+            q = tf.transpose(q, perm=[0, 2, 1]) # (batch_size, hidden_size, max_q)
             tf.summary.histogram("Q'", q)
 
-            Q = tf.tanh(batch_linear(q, self.hidden_size, True))
-            tf.summary.histogram("Q", Q)   
+            Q = tf.tanh(batch_linear(q, self.hidden_size, True)) # (batch_size, hidden_size, max_q)
+            tf.summary.histogram('Q', Q)   
             
         print('D:', D.get_shape())
         print('Q:', Q.get_shape())
 
         with tf.variable_scope('affinity_mat'):
-            L = tf.matmul(tf.transpose(D, perm=[0, 2, 1]), Q, name='L')
+            L = tf.matmul(D, Q, name='L', transpose_a=True) # (batch_size, max_x, max_q)
+            tf.summary.histogram('L', L)
 
         print('L:', L.get_shape())
 
         with tf.variable_scope('normalize_aff'):
-            Aq = tf.nn.softmax(L, name='Aq')
-            Ad = tf.nn.softmax(tf.transpose(L, perm=[0, 2, 1]), name='Ad')
-
+            Aq = tf.nn.softmax(L, name='Aq') # (batch_size, max_x, max_q)
+            Ad = tf.nn.softmax(tf.transpose(L, perm=[0, 2, 1]), name='Ad') # (batch_size, max_q, max_x)
+            tf.summary.histogram('Aq', Aq)
+            tf.summary.histogram('Ad', Ad)
+        
         print('Aq:', Aq.get_shape())
         print('Ad:', Ad.get_shape())
 
         with tf.variable_scope('attention_contexts'):
-            Cq = tf.matmul(D, Aq, name='Cq')
+            Cq = tf.matmul(D, Aq, name='Cq') # (batch_size, hidden_size, max_q)
 
         print('Cq:', Cq.get_shape())
 
         with tf.variable_scope('attention_questions'):
-            Cd = tf.concat([Q, Cq], axis=1)
-            Cd = tf.matmul(Cd, Ad, name='Cd')
+            Cd = tf.concat([Q, Cq], axis=1) # (batch_size, 2*hidden_size, max_q)
+            Cd = tf.matmul(Cd, Ad, name='Cd') # (batch_size, 2*hidden_size, max_x)
         
         print('Cd:', Cd.get_shape())
 
-        co_att = tf.concat([D, Cd], axis=1)
-        co_att = tf.transpose(co_att, perm=[0, 2, 1])
+        co_att = tf.concat([D, Cd], axis=1) # (batch_size, 3*hidden_size, max_x)
+        co_att = tf.transpose(co_att, perm=[0, 2, 1]) # (batch_size, max_x, 3*hidden_size)
 
         print('co_att:', co_att.get_shape())
         
@@ -81,9 +84,9 @@ class Model:
             lstm_fw_cell = DropoutWrapper(lstm_fw_cell, input_keep_prob=keep_prob)
             lstm_bw_cell = DropoutWrapper(lstm_bw_cell, input_keep_prob=keep_prob)
 
-            u, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs=co_att, sequence_length=x_len, dtype=tf.float32)
+            u, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, inputs=co_att, sequence_length=x_len, dtype=tf.float32) # (batch_size, max_x, hidden_size)
     
-            U = tf.concat(u, axis=2)
+            U = tf.concat(u, axis=2) # (batch_size, max_x, 2*hidden_size)
         
         print('U:', U.get_shape())
 
@@ -97,25 +100,25 @@ class Model:
             
             batch_size = tf.shape(U)[0]
 
-            U_trans = tf.transpose(U, perm=[1, 0, 2])
+            U_trans = tf.transpose(U, perm=[1, 0, 2]) # (max_q, batch_size, 2*hidden_size) //the u_t vectors
 
             # batch indices
             loop_until = tf.range(0, batch_size, dtype=tf.int32)
 
             # initial estimated positions
-            s = tf.zeros([batch_size], dtype=tf.int32)
-            e = tf.zeros([batch_size], dtype=tf.int32)
+            s = tf.zeros([batch_size], dtype=tf.int32) # (batch_size)
+            e = tf.zeros([batch_size], dtype=tf.int32) # (batch_size)
 
             print('s:', s.get_shape())
             print('e:', e.get_shape())
 
             # Get U vectors of starting indexes
             fn = lambda idx: select(U, s, idx)
-            u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32) # (batch_size, 2*hidden_size)
 
             # Get U vectors of ending indexes
             fn = lambda idx: select(U, e, idx)
-            u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+            u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32) # (batch_size, 2*hidden_size)
 
         print('u_s:', u_s.get_shape())
         print('u_e:', u_e.get_shape())
@@ -124,6 +127,8 @@ class Model:
             highway_alpha = highway_maxout(self.hidden_size, self.pool_size)
             highway_beta = highway_maxout(self.hidden_size, self.pool_size)
 
+        self._s, self._e = [], []
+        self._alpha, self._beta = [], []
         with tf.variable_scope('decoder') as scope:
             # LSTM for decoding
             lstm_dec = LSTMCell(self.hidden_size)
@@ -132,10 +137,10 @@ class Model:
                 if step > 0:
                     scope.reuse_variables()
                 # single step lstm
-                _input = tf.concat([u_s, u_e], axis=1)
+                _input = tf.concat([u_s, u_e], axis=1) # (batch_size, 4*hidden_size)
                 print('_input:', _input.get_shape())
 
-                _, h = tf.contrib.rnn.static_rnn(lstm_dec, [_input], dtype=tf.float32)
+                _, h = tf.contrib.rnn.static_rnn(lstm_dec, [_input], dtype=tf.float32) # (batch_size, hidden_size)
                
                 h_state = h[0] # h_state = tf.concat(h, axis=1)
                 print('h_state:', h_state.get_shape())
@@ -143,42 +148,45 @@ class Model:
                 with tf.variable_scope('highway_alpha'):
                     # compute start position first
                     fn = lambda u_t: highway_alpha(u_t, h_state, u_s, u_e)
-                    alpha = tf.map_fn(lambda u_t: fn(u_t), U_trans, dtype=tf.float32) # u_t is full U matrix for each batch
-                    s = tf.reshape(tf.argmax(alpha, axis=0), [batch_size])
+                    # for each t, send in (batch_size, hidden_size) matrix 
+                    alpha = tf.map_fn(lambda u_t: fn(u_t), U_trans, dtype=tf.float32) # (max_x, batch_size, 1, 1)
+                    s = tf.reshape(tf.argmax(alpha, axis=0), [batch_size]) # (batch_size)
                     
                     # update start guess
                     fn = lambda idx: select(U, s, idx)
-                    u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+                    u_s = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32) # (batch_size, 200)
                     print('u_s:', u_s.get_shape())
 
                 with tf.variable_scope('highway_beta'):
                     # compute end position next
                     fn = lambda u_t: highway_beta(u_t, h_state, u_s, u_e)
-                    beta = tf.map_fn(lambda u_t: fn(u_t), U_trans, dtype=tf.float32)
-                    e = tf.reshape(tf.argmax(beta, axis=0), [batch_size])
+                    beta = tf.map_fn(lambda u_t: fn(u_t), U_trans, dtype=tf.float32) # (max_x, batch_size, 1, 1)
+                    e = tf.reshape(tf.argmax(beta, axis=0), [batch_size]) # (batch_size)
                     
                     # update end guess
                     fn = lambda idx: select(U, e, idx)
-                    u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32)
+                    u_e = tf.map_fn(lambda idx: fn(idx), loop_until, dtype=tf.float32) # (batch_size, 200)
                     print('u_e:', u_e.get_shape())
 
-        q_mask = tf.sequence_mask(q_len, self.max_q)
-        x_mask = tf.sequence_mask(x_len, self.max_x)
-
-        # logits
-        with tf.variable_scope('start_index'):
-            val = tf.reshape(alpha, [batch_size, -1])
-            logits_start = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
-            yp_start = tf.argmax(logits_start, axis=1, name='starting_index')
-            tf.summary.histogram("yp_start", yp_start)
-
-        with tf.variable_scope('end_index'):
-            val = tf.reshape(beta, [batch_size, -1])
-            logits_end = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
-            yp_end = tf.argmax(logits_end, axis=1, name='ending_index')
-            tf.summary.histogram("yp_end", yp_end)
-
-        outputs = {'logits_start': logits_start, 'logits_end': logits_end, 'yp_start': yp_start, 'yp_end': yp_end}
-        return outputs
-
+                self._s.append(s)
+                self._e.append(e)
+                self._alpha.append(tf.reshape(alpha, [batch_size, -1]))
+                self._beta.append(tf.reshape(beta, [batch_size, -1]))
+        
+        self.loss = self._loss_multitask(self._alpha, y_begin, self._beta, y_end)
+        self.merged_summary = tf.summary.merge_all()
     
+
+    def _loss_multitask(self, logits_alpha, labels_alpha, logits_beta, labels_beta):
+        '''Cumulative loss for start and end positions.'''
+        fn = lambda logit, label: self._loss_shared(logit, label)
+        loss_alpha = [fn(alpha, labels_alpha) for alpha in logits_alpha]
+        loss_beta = [fn(beta, labels_beta) for beta in logits_beta]
+        return tf.reduce_sum([loss_alpha, loss_beta], name='loss')
+
+    def _loss_shared(self, logits, labels):
+        # labels = tf.reshape(labels, [self._params.batch_size])
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels, name='per_step_cross_entropy')
+        cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+        tf.add_to_collection('per_step_losses', cross_entropy_mean)
+        return tf.add_n(tf.get_collection('per_step_losses'), name='per_step_loss')
