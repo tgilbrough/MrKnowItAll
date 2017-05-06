19fdb9dbdd34bf3dc8335d45ff6c2a7437b98a1d
TJ Gilbrough <tgilbrough@yahoo.com>
2017-05-06 12:23:17 -0700

    Initial progress on neural approach to passage relevance

---
+++ baseline_model_pv.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+from tensorflow.contrib.rnn import DropoutWrapper
+from tensorflow.contrib.rnn import GRUCell
+
+
+class Model:
+    def __init__(self, config, max_x, max_q):
+        self.model_name = 'baseline'
+        self.dim = config.hidden_size
+        self.max_x = max_x
+        self.max_q = max_q
+        self.saver = None
+
+    def build(self, xs, x_len, q, q_len, y, embeddings, keep_prob):
+        with tf.variable_scope('embedding_matrix'):
+            # embeddings matrix, may be memory ineffecient (Fix)
+            emb_mat = tf.get_variable(name='emb_mat', shape=embeddings.shape, initializer=tf.constant_initializer(embeddings), trainable=False)
+            tf.summary.histogram('emb_mat', emb_mat)
+
+        with tf.variable_scope('embedding_passages'):
+            context = []
+            
+            for x in xs:
+                context.append(tf.nn.embedding_lookup(emb_mat, x, name='context'))
+
+        with tf.variable_scope('embedding_question'):
+            question = tf.nn.embedding_lookup(emb_mat, q, name='question')
+
+        with tf.variable_scope('encoding_passages') as scope:
+            gru_c_cell = GRUCell(self.dim)
+            gru_c_cell = DropoutWrapper(gru_c_cell, input_keep_prob=keep_prob)  # to avoid over-fitting
+
+            context_output = []
+            for i in range(len(context)):
+                if i > 0:
+                    scope.reuse_variables()
+                outputs_context, _ = tf.nn.bidirectional_dynamic_rnn(gru_c_cell, gru_c_cell, inputs=context[i], sequence_length=x_len, dtype=tf.float32)
+                context_fw, context_bw = outputs_context
+                context_output.append(tf.concat([context_fw, context_bw], axis=2))
+                # tf.summary.histogram('context_output', context_output)
+            context_output = tf.concat(context_output, axis=2)
+
+        with tf.variable_scope('encoding_question'):
+            gru_q_cell = GRUCell(self.dim)
+            gru_q_cell = DropoutWrapper(gru_q_cell, input_keep_prob=keep_prob)  # to avoid over-fitting
+
+            # tf.get_variable_scope().reuse_variables()
+            outputs_question, _ = tf.nn.bidirectional_dynamic_rnn(gru_q_cell, gru_q_cell, inputs=question, sequence_length=q_len, dtype=tf.float32)
+            question_fw, question_bw = outputs_question
+            question_output = tf.concat([question_fw, question_bw], axis=2)
+            tf.summary.histogram('question_output', question_output)
+
+        with tf.variable_scope('question_tiling'):
+            q_mask = tf.sequence_mask(q_len, self.max_q)
+            mask = tf.expand_dims(q_mask, -1)
+            q_temp = question_output * tf.cast(mask, 'float')
+            q_avg = tf.reduce_mean(q_temp, axis=1)
+            q_avg_exp = tf.expand_dims(q_avg, axis=1)
+            q_avg_tiled = tf.tile(q_avg_exp, [1, self.max_x, 1])
+            tf.summary.histogram('q_avg_tiled', q_avg_tiled)
+
+        xq = tf.concat([context_output, q_avg_tiled], axis=2)
+        tf.summary.histogram('xq', xq)
+
+        with tf.variable_scope('post_process'):
+            gru_xq_cell = GRUCell(self.dim)
+            gru_xq_cell = DropoutWrapper(gru_xq_cell, input_keep_prob=keep_prob)  # to avoid over-fitting
+
+            outputs_xq, _ = tf.nn.bidirectional_dynamic_rnn(gru_xq_cell, gru_xq_cell, inputs=xq, sequence_length=x_len, dtype=tf.float32)
+            xq_fw, xq_bw = outputs_xq
+            xq_output = tf.concat([xq_fw, xq_bw], axis=2)
+            tf.summary.histogram('xq_output', xq_output)
+
+        # Get rid of the sequence dimension
+        xq_flat = tf.reshape(xq_output, [-1, 2 * self.dim])
+
+        # tensor of boolean values of max_x length and True in first x_len indices
+        x_mask = tf.sequence_mask(x_len, self.max_x)
+
+        # logits
+        with tf.variable_scope('relevance'):
+            val = tf.reshape(tf.layers.dense(inputs=xq_flat, units=10), [-1, self.max_x])
+            logits = val - (1.0 - tf.cast(x_mask, 'float')) * 10.0e10
+
+        with tf.variable_scope('loss'):
+            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits), name='loss')
+
+        tf.summary.scalar('loss', loss)
+
+        self.loss = loss
+        self.merged_summary = tf.summary.merge_all()
---
+++ data_pv.py
@@ -0,0 +1,235 @@
+import json, math
+import numpy as np
+import os
+import nltk
+nltk.download('punkt')
+
+import tensorflow as tf
+
+class Data:
+    def __init__(self, config):
+        self.batch_size = config.batch_size
+        self.keep_prob = config.keep_prob
+        self.valBatchNum = 0        
+
+        # load training data, parse, and split
+        print('Loading in training data...')
+        trainData = self.importMsmarco(config.train_path)
+        self.tContext, self.tQuestion, self.tY, self.maxLenTContext, \
+                                     self.maxLenTQuestion, self.maxTPassages = self.splitMsmarcoDatasets(trainData)
+
+        # load validation data, parse, and split
+        print('Loading in validation data...')
+        valData = self.importMsmarco(config.val_path)
+        self.vContext, self.vQuestion, self.vY, self.maxLenVContext, self.maxLenVQuestion, \
+                                        self.maxVPassages = self.splitMsmarcoDatasets(valData)
+
+        print('Building vocabulary...')
+        # build a vocabulary over all training and validation context paragraphs and question words
+        vocab = self.buildVocab([c for x in self.tContext for c in x] + self.tQuestion + [c for x in self.vContext for c in x] + self.vQuestion)
+
+        # Reserve 0 for masking via pad_sequences
+        self.vocab_size = len(vocab) + 1
+        word_index = dict((c, i + 1) for i, c in enumerate(vocab))
+        self.max_context_size = max(self.maxLenTContext, self.maxLenVContext)
+        self.max_ques_size = max(self.maxLenTQuestion, self.maxLenVQuestion)
+        self.max_passages = max(self.maxTPassages, self.maxVPassages)
+
+        # Note: Need to download and unzip Glove pre-train model files into same file as this script
+        print('Preparing embedding matrix.')
+        embeddings_index = self.loadGloveModel('./datasets/glove/glove.6B.' + str(config.emb_size) + 'd.txt')
+        self.embeddings = self.createEmbeddingMatrix(embeddings_index, word_index)
+
+        # vectorize training and validation datasets
+        print('Begin vectorizing process...')
+
+        # tX: training Context, tXq: training Question, tYBegin: training Answer Begin ptr,
+        # tYEnd: training Answer End ptr
+        self.tX, self.tXq = self.vectorizeData(self.tContext, self.tQuestion, word_index)
+        self.vX, self.vXq = self.vectorizeData(self.vContext, self.vQuestion, word_index)
+
+        print('Vectorizing process completed.')
+
+        self.convertToNumpy()
+
+    def getAllData(self):
+        return self.all_data
+
+    def convertToNumpy(self):
+        self.tX = np.array(self.tX)
+        self.tXq = np.array(self.tXq)
+        self.tY = np.array(self.tY)
+        self.vX = np.array(self.vX)
+        self.vXq = np.array(self.vXq)
+        self.vY = np.array(self.vY)
+
+    def getNumTrainBatches(self):
+        return int(math.ceil(len(self.tX) / self.batch_size))
+
+    def getNumValBatches(self):
+        return int(math.ceil(len(self.vX) / self.batch_size))
+
+    def getRandomTrainBatch(self):
+        points = np.random.choice(len(self.tX), self.batch_size)
+
+        tX_batch = self.tX[points]
+        tXq_batch = self.tXq[points]
+        tY_batch = self.tY[points]
+
+        return {'tX': tX_batch, 'tXq': tXq_batch, 'tY': tY_batch}
+
+    def getRandomValBatch(self):
+        points = np.random.choice(len(self.vX), self.batch_size)
+
+        vX_batch = self.vX[points]
+        vXq_batch = self.vXq[points]
+        vY_batch = self.vY[points]
+
+        return {'vX': vX_batch, 'vXq': vXq_batch, 'vY': vY_batch}
+
+    def getValBatch(self):
+        start = self.valBatchNum * self.batch_size
+        end = min(len(self.vX), (self.valBatchNum + 1) * self.batch_size)
+        points = np.arange(start, end)
+
+        vX_batch = self.vX[points]
+        vXq_batch = self.vXq[points]
+        vY_batch = self.vY[points]
+
+        self.valBatchNum += 1
+
+        if self.valBatchNum >= self.getNumValBatches():
+            self.valBatchNum = 0
+
+        return {'vX': vX_batch, 'vXq': vXq_batch, 'vY': vY_batch}
+
+    def loadGloveModel(self, gloveFile):
+        print("Loading Glove Model...")
+        f = open(gloveFile,'r', encoding='utf-8')
+        embedding_index = {}
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embedding_index[word] = coefs
+        f.close()
+        print('Found %s word vectors.' % len(embedding_index))
+        return embedding_index
+
+    def createEmbeddingMatrix(self, embeddings_index, word_index):
+        dim_size = len(embeddings_index['a'])
+        # +1 is for <PAD>
+        embedding_matrix = np.zeros((len(word_index) + 1, dim_size))
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+        return embedding_matrix
+
+    def tokenize(self, sent):
+        '''Return the tokens of a context including punctuation. Wrapper around nltk.word_tokenize to
+           fix weird quotation marks.
+        '''
+        return [token.replace("``", '"').replace("''", '"') for token in nltk.word_tokenize(sent)]
+
+    def join(self, sent):
+        def join_punctuation(seq, characters='.,;?!'):
+            characters = set(characters)
+            seq = iter(seq)
+            current = next(seq)
+
+            for nxt in seq:
+                if nxt in characters:
+                    current += nxt
+                else:
+                    yield current
+                    current = nxt
+
+            yield current
+        return ' '.join(join_punctuation(sent))
+
+    
+
+    def splitMsmarcoDatasets(self, f):
+        '''Given a parsed Json data object, split the object into training context (paragraph), question, answer matrices,
+           and keep track of max context and question lengths.
+        '''
+        xContext = [] # list of list of contexts paragraphs
+        xQuestion = [] # list of questions
+        xSelected = [] # list of list of selected values
+        maxLenContext = 0
+        maxLenQuestion = 0
+        maxPassages = 0
+
+        for sample in f['data']:
+            passages = []
+            selected = []
+            for passage in sample['passages']:
+                context = passage['passage_text']
+                context = context.replace("''", '" ')
+                context = context.replace("``", '" ')
+                contextTokenized = self.tokenize(context.lower())
+                contextLength = len(contextTokenized)
+                if contextLength > maxLenContext:
+                    maxLenContext = contextLength
+                passages.append(contextTokenized)
+                selected.append(passage['is_selected'])
+
+            if len(passages) > maxPassages:
+                maxPassages = len(passages)
+
+            xContext.append(passages)
+            xSelected.append(selected)
+
+            question = sample['query']
+            question = question.replace("''", '" ')
+            question = question.replace("``", '" ')
+            questionTokenized = self.tokenize(question.lower())
+            if len(questionTokenized) > maxLenQuestion:
+                maxLenQuestion = len(questionTokenized)
+            xQuestion.append(questionTokenized)
+
+        for i in range(len(xContext)):
+            for j in range(maxPassages - len(xContext[i])):
+                xContext[i].append([])
+                xSelected.append(0)
+
+        return xContext, xQuestion, xSelected, maxLenContext, maxLenQuestion, maxPassages
+
+    def vectorizeData(self, xContext, xQuestion, word_index):
+        '''Converts context and question words to their respective index and pad context to max context length
+           and question to max question length. *Convert answers to one-hot vectors of length max context.
+        '''
+        X = []
+        Xq = []
+        for i in range(len(xContext)):
+            x = []
+            for j in range(len(xContext[i])):
+                x.append([word_index[w] for w in xContext[i][j]])
+            xq = [word_index[w] for w in xQuestion[i]]
+            # map the first and last words of answer span to one-hot representations
+            X.append(x)
+            Xq.append(xq)
+        return [self.pad_sequences(x, self.max_context_size) for x in X], self.pad_sequences(Xq, self.max_ques_size)
+
+    def pad_sequences(self, X, maxlen):
+        for context in X:
+            for i in range(maxlen - len(context)):
+                context.append(0)
+        return X
+
+    def importMsmarco(self, json_file):
+        data = {}
+        data['data'] = []
+        with open(json_file, encoding='utf-8') as f:
+            data['data'] = [json.loads(line) for line in f]
+        return data
+
+    def buildVocab(self, sentences):
+        '''Accepts a list of list of words. For example, a list of contexts or questions that are tokenized.
+           Returns a sorted list of strings that comprise the vocabulary.
+        '''
+        vocab = set([word for words in sentences for word in words])
+        vocab = sorted(vocab)
+        return vocab
