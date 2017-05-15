2a4744918eb5d7868158c120eb27ae93f818d4f5
TJ Gilbrough <tgilbrough@yahoo.com>
2017-05-14 21:53:12 -0700

    Adding training flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import tensorflow as tf
 from tqdm import tqdm
 import os
+import sys
 
 import baseline_model
 import attention_model
@@ -13,6 +14,7 @@ from data import Data
 
 def get_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--train', '-t', type=int, default=1)
     parser.add_argument('--keep_prob', '-kp', type=float, default=0.5)
     parser.add_argument('--hidden_size', '-hs', type=int, default=50)
     parser.add_argument('--emb_size', '-es', type=int, default=50) # this could be 50 (171.4 MB), 100 (347.1 MB), 200 (693.4 MB), or 300 (1 GB)
@@ -102,49 +104,54 @@ def main():
 
         sess.run(tf.global_variables_initializer())
 
-        for e in range(config.epochs):
-            print('Epoch {}/{}'.format(e + 1, config.epochs))
-            for i in tqdm(range(number_of_train_batches)):
-                trainBatch = data.getRandomTrainBatch()
+        if config.train:
+            for e in range(config.epochs):
+                print('Epoch {}/{}'.format(e + 1, config.epochs))
+                for i in tqdm(range(number_of_train_batches)):
+                    trainBatch = data.getRandomTrainBatch()
 
+                    feed_dict={x: trainBatch['tX'],
+                                x_len: [len(trainBatch['tX'][i]) for i in range(len(trainBatch['tX']))],
+                                q: trainBatch['tXq'],
+                                q_len: [len(trainBatch['tXq'][i]) for i in range(len(trainBatch['tXq']))],
+                                y_begin: trainBatch['tYBegin'],
+                                y_end: trainBatch['tYEnd'],
+                                keep_prob: config.keep_prob}
+                    sess.run(train_step, feed_dict=feed_dict)
+
+                # Record results for tensorboard, once per epoch
                 feed_dict={x: trainBatch['tX'],
-                            x_len: [len(trainBatch['tX'][i]) for i in range(len(trainBatch['tX']))],
-                            q: trainBatch['tXq'],
-                            q_len: [len(trainBatch['tXq'][i]) for i in range(len(trainBatch['tXq']))],
-                            y_begin: trainBatch['tYBegin'],
-                            y_end: trainBatch['tYEnd'],
-                            keep_prob: config.keep_prob}
-                sess.run(train_step, feed_dict=feed_dict)
+                        x_len: [len(trainBatch['tX'][i]) for i in range(len(trainBatch['tX']))],
+                        q: trainBatch['tXq'],
+                        q_len: [len(trainBatch['tXq'][i]) for i in range(len(trainBatch['tXq']))],
+                        y_begin: trainBatch['tYBegin'],
+                        y_end: trainBatch['tYEnd'],
+                        keep_prob: 1.0}
+                train_sum = sess.run(model.merged_summary, feed_dict=feed_dict)
 
-            # Record results for tensorboard, once per epoch
-            feed_dict={x: trainBatch['tX'],
-                    x_len: [len(trainBatch['tX'][i]) for i in range(len(trainBatch['tX']))],
-                    q: trainBatch['tXq'],
-                    q_len: [len(trainBatch['tXq'][i]) for i in range(len(trainBatch['tXq']))],
-                    y_begin: trainBatch['tYBegin'],
-                    y_end: trainBatch['tYEnd'],
-                    keep_prob: 1.0}
-            train_sum = sess.run(model.merged_summary, feed_dict=feed_dict)
+                valBatch = data.getRandomValBatch()
+                feed_dict={x: valBatch['vX'],
+                        x_len: [len(valBatch['vX'][i]) for i in range(len(valBatch['vX']))],
+                        q: valBatch['vXq'],
+                        q_len: [len(valBatch['vXq'][i]) for i in range(len(valBatch['vXq']))],
+                        y_begin: valBatch['vYBegin'],
+                        y_end: valBatch['vYEnd'],
+                        keep_prob: 1.0}
+                val_sum, val_loss  = sess.run([model.merged_summary, model.loss], feed_dict=feed_dict)
+                if val_loss < min_val_loss:
+                    saver.save(sess, save_model_path + '/model')
+                    min_val_loss = val_loss
 
-            valBatch = data.getRandomValBatch()
-            feed_dict={x: valBatch['vX'],
-                    x_len: [len(valBatch['vX'][i]) for i in range(len(valBatch['vX']))],
-                    q: valBatch['vXq'],
-                    q_len: [len(valBatch['vXq'][i]) for i in range(len(valBatch['vXq']))],
-                    y_begin: valBatch['vYBegin'],
-                    y_end: valBatch['vYEnd'],
-                    keep_prob: 1.0}
-            val_sum, val_loss  = sess.run([model.merged_summary, model.loss], feed_dict=feed_dict)
-            if val_loss < min_val_loss:
-                saver.save(sess, save_model_path + '/model')
-                min_val_loss = val_loss
-
-            train_writer.add_summary(train_sum, e)
-            val_writer.add_summary(val_sum, e)
+                train_writer.add_summary(train_sum, e)
+                val_writer.add_summary(val_sum, e)
 
         # Load best graph on validation data
-        new_saver = tf.train.import_meta_graph(save_model_path + '/model.meta')
-        new_saver.restore(sess, tf.train.latest_checkpoint(save_model_path))
+        try:
+            new_saver = tf.train.import_meta_graph(save_model_path + '/model.meta')
+            new_saver.restore(sess, tf.train.latest_checkpoint(save_model_path))
+        except:
+            print('Must train model first with --train flag')
+            sys.exit()
 
         # Print out answers for one of the batches
         vContext = []
